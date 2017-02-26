81c81ff6f5fbc801c6c9e2221154562e3202abeb
Philip Lombardi <plombardi@datawire.io>
2017-02-26 18:53:50 -0500

    M1: Update World to hold a ref to a pre-existing AWS VPC ID and also a Terraform repository

--- src/main/kotlin/io/datawire/deployd/api/World.kt
+++ src/main/kotlin/io/datawire/deployd/api/World.kt
@@ -6,4 +6,5 @@ import io.datawire.deployd.cloud.AwsProvider
 
 data class World(@JsonProperty val name: String,
                  @JsonProperty val description: String,
-                 @JsonProperty val amazon: AwsProvider)
+                 @JsonProperty val amazon: AwsProvider, // TODO: abstract this to a generic providers collection
+                 @JsonProperty val terraform: String)
--- src/main/kotlin/io/datawire/deployd/cloud/AwsProvider.kt
+++ src/main/kotlin/io/datawire/deployd/cloud/AwsProvider.kt
@@ -5,4 +5,17 @@ import com.fasterxml.jackson.annotation.JsonProperty
 
 data class AwsProvider(@JsonProperty val accessKey: String?,
                        @JsonProperty val secretKey: String?,
-                       @JsonProperty val region: String)
+                       @JsonProperty val region: String,
+                       @JsonProperty val network: AwsNetwork) {
+
+    fun toTerraformProvider(): Map<String, Any?> {
+        return mapOf("aws" to mapOf(
+                "region" to region,
+                "accessKey" to accessKey,
+                "secretKey" to secretKey
+        ))
+    }
+}
+
+data class AwsNetwork(@JsonProperty val id: String,
+                      @JsonProperty val subnets: List<String>)
